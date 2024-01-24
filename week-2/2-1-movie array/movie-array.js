8584d918bc1f2c5a1fefdbc8c9cf436726940e48
Morgen Ruff <morgen.ruff@gmail.com>
2024-01-23 19:59:47 -0600

    minor updates

--- week-2/2-1-movie array/movie-array.js
+++ week-2/2-1-movie array/movie-array.js
@@ -1,17 +1,10 @@
-const favoriteMovies = [
+const morgensMovies = [
 	'Close-Up',
 	'Johnny Guitar',
 	'Beau Travail',
 	'Days of Heaven',
 	'Heat'
 ];
-
-console.log(favoriteMovies);
-
-const yourFavoriteMovie = prompt('What is your favorite movie?');
-
-console.log(yourFavoriteMovie);
-
-favoriteMovies.push(yourFavoriteMovie);
-
-console.log('Our combined favorite movies are ' + favoriteMovies.toString() + '. Thanks for adding to my list!');
+const yourFavoriteMovie = window.prompt('What is your favorite movie?');
+morgensMovies.push(yourFavoriteMovie);
+console.log('Our combined favorite movies are ' + morgensMovies.toString() + '. Thanks for adding to the list!');
