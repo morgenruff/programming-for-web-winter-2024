e261d31a4b1665da3f9dd8c6801f7f7c7c952770
Morgen Ruff <morgen.ruff@gmail.com>
2024-01-25 12:13:39 -0600

    setup exercise 2.2 + minor updates to config

---
+++ week-2/2-2-objects-in-arrays/objects-in-arrays.js
@@ -0,0 +1,20 @@
+// Description:
+// 
+// You are going to combine your knowledge of arrays with your knowledge of objects to create a (drum-roll please) array of objects! 🎉
+// 
+// Steps:
+// 
+// Create an array with your favorite games as objects inside of the array. Each game object will represent one game.
+// 
+// Each object will have the same properties. I recommend title, type, numberOfPlayers, rating, shortDescription. Please note, the naming convention of object properties is camelCase.
+// 
+// You must have 3 or more games in the collection
+// 
+// Prompt the user with a message like "I have 3 games in my collection. Pick a number between 1 and 3 and I'll tell you about that game"
+// 
+// Cast the prompt to a number (if possible)
+// 
+// Using the plus sign as a concatenation operator, alert the user to you game. "You selected" + myGameArray[promptResponse -1 ].title + " which is a " + myGameArray[promptResponse - 1].type + " of game..." And so forth.
+// 
+// Add your exercise files to your git repo and commit your changes. Push to github. Upload to your MCAD hosting and share a link to that url in this discussion. Ensure that URL is accessible and functional.
+
