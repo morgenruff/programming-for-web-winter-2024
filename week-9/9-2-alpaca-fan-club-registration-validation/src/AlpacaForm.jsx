37c5313d4037731d91322d08aa751a5c89d45f1e
Morgen Ruff <morgen.ruff@gmail.com>
2024-03-21 22:55:54 -0400

    9.2 complete, build, update homepage

---
+++ week-9/9-2-alpaca-fan-club-registration-validation/src/AlpacaForm.jsx
@@ -0,0 +1,267 @@
+import { useState } from 'react'; // import useState
+import './AlpacaForm.css'; // import CSS
+export function AlpacaForm () {
+  const initialFormData = {
+    firstName: '',
+    lastName: '',
+    street: '',
+    city: '',
+    state: '',
+    country: '',
+    zipCode: '',
+    colors: [],
+  } // object array of input form data
+  const [formObject, setFormObject] = useState(initialFormData); // core useState values
+  const [errorObject, setErrorObject] = useState({
+    firstName: '', // false value
+    country: '', // false value
+  }); // error object variable
+  const states = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Carolina',
+    'North Dakota',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming']; // list of states
+  function changeHandler (event) {
+    console.log(event.target.value);
+    if (event.target.name === 'colors') {
+      const colorName = event.target.id; // calls id on color checkbox option
+      if (formObject.colors.includes(colorName)) { // check for chosen colorName
+        setFormObject({
+          ...formObject, // call initial formObject.colors with spread operator
+          colors: formObject.colors.filter((color) => { // filter out un-chosen colors
+            color !== colorName
+          })
+        })
+      } else {
+        setFormObject({
+          ...formObject,
+          colors: [...formObject.colors, colorName] // set color to user-chosen color
+        })
+      }
+    } else {
+      setFormObject((previousForm => {
+        return {
+          ...previousForm, // call previous form value
+          [event.target.name]: event.target.value, // overwrite existing value by calling key from initial data array
+        }
+      }))
+    }
+  } // handle user-created changes to the form
+  function validateFirstName() {
+    if (!formObject.firstName) { // if first name isn't populated, return required error message
+      setErrorObject({
+        ...errorObject,
+        firstName: 'this field is required'
+      })
+    } else { // else return empty string
+      setErrorObject({
+        ...errorObject,
+        firstName: ''
+      })
+    }
+  }
+  function validateLastName() {
+      if (!formObject.lastName) { // if last name isn't populated, return required error message
+        setErrorObject({
+          ...errorObject,
+          lastName: 'this field is required'
+        })
+      } else { // else return empty string
+        setErrorObject({
+          ...errorObject,
+          lastName: ''
+        })
+      }
+    }
+  function handleSubmit (event) { // handle user form submission
+    event.preventDefault(); // prevent page refresh on change
+    validateFirstName(); // run validation on first name
+    validateLastName(); // run validation on last name
+    console.log('the whole form object', formObject); // log out the entire submission on user submit
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Personal Information</legend>
+          <div className="formGroup">
+            <label className="required" htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              onChange={changeHandler} // when input changes (type), use changeHandler function
+              value={formObject.firstName} // comes from state variable
+              onBlur={validateFirstName}
+            />
+            {errorObject.firstName && (
+              <>
+                <br/>
+                <small>{errorObject.firstName}</small>
+              </>
+            )}
+          </div>
+          <div className="formGroup">
+            <label className="required" htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              onChange={changeHandler}
+              value={formObject.lastName}
+              onBlur={validateLastName}
+            />
+            {errorObject.lastName && (
+              <>
+                <br/>
+                <small>{errorObject.lastName}</small>
+              </>
+            )}
+          </div>
+          <div className="formGroup">
+            <label htmlFor="street">Street Address</label>
+            <input
+              type="text"
+              name="street"
+              id="street"
+              onChange={changeHandler}
+              value={formObject.street}
+            />
+          </div>
+          <div className="formGroup">
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              onChange={changeHandler}
+              value={formObject.city}
+            />
+          </div>
+          <div className="formGroup">
+            <label htmlFor="state">State</label>
+            <select name="state" id="state" value={formObject.state} onChange={changeHandler}>
+              {states.map((singleState, index) => {
+                return (
+                  <option key={index} value={singleState}>{singleState}</option>
+                )
+              })}
+            </select>
+          </div>
+          <div className="formGroup">
+            <label htmlFor="city">Zip Code</label>
+            <input
+              type="text"
+              name="zipCode"
+              id="zipCode"
+              onChange={changeHandler}
+              value={formObject.zipCode}
+            />
+          </div>
+          <div className="formGroup">
+            <label htmlFor="country">Country</label>
+            <select name="country" id="country" value={formObject.country} onChange={changeHandler}>
+              <option value="US">US</option>
+              <option value="Canada">Canada</option>
+            </select>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Tell us all about your alpaca interests!</legend>
+          <p>Favorite Alpaca Colors</p>
+          <div className="formGroup">
+            <label htmlFor="brown">
+              Brown
+              <input
+                type="checkbox"
+                name="colors"
+                id="brown"
+                checked={formObject.colors.includes('brown')}
+                onChange={changeHandler}
+              />
+            </label>
+            <label htmlFor="black">
+              Black
+              <input
+                type="checkbox"
+                name="colors"
+                id="black"
+                checked={formObject.colors.includes('black')}
+                onChange={changeHandler}
+              />
+            </label>
+            <label htmlFor="beige">
+              Beige
+              <input
+                type="checkbox"
+                name="colors"
+                id="beige"
+                checked={formObject.colors.includes('beige')}
+                onChange={changeHandler}
+              />
+            </label>
+            <label htmlFor="pinto">
+              Pinto
+              <input
+                type="checkbox"
+                name="colors"
+                id="pinto"
+                checked={formObject.colors.includes('pinto')}
+                onChange={changeHandler}
+              />
+            </label>
+          </div>
+        </fieldset>
+        {/* for the button, disable click if validation detects errors, otherwise allow submit */}
+        <button type='submit' disabled={errorObject.firstName || errorObject.country}>&#129433; SIGN ME UP! &#129433;</button>
+      </form>
+    </>
+  )
+}
---
+++ week-9/9-2-alpaca-fan-club-registration-validation/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { AlpacaForm } from './AlpacaForm.jsx';
+
+function App() {
+
+  return (
+    <>
+      <h1>Alpaca Fan Club Registration</h1>
+      <AlpacaForm />
+    </>
+  )
+}
+
+export default App
