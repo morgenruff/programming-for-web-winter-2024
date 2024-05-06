8624e14c9bd190985142dc684182815864f878dc
Morgen Ruff <morgen.ruff@gmail.com>
2024-05-05 21:20:39 -0500

    many failures...

---
+++ week-14-15/final-climate-data/src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { ReactP5Wrapper } from '@p5-wrapper/react';
+import './App.css'
+function App() {
+  return (
+    <>
+      Hi
+    </>
+  )
+}
+
+export default App()
--- week-14-15/fp-climate-data-visualization/src/p5-visualizations/co2.js
+++ week-14-15/fp-climate-data-visualization/src/p5-visualizations/co2.js
@@ -1,17 +1,27 @@
+// noinspection SpellCheckingInspection
+
+// loop through JSON data to get single average PPM value - 'avg' by month (number)
+// loop through JSON data to get date value (string)
+// loop through JSON data to get trend value (number)
+// create cells numbered based on each JSON object, simple index (not drilling to key-value pair)
+// color cells based on 'avg' value - access color array with an if statement (e.g., if (avg > 390 && avg < 400) color cell)
+// run through date values and display in a HUD somewhere next to the grid at the same framerate as the cell drawing
+// create slider to adjust framerate
+// push button to start
+// push button to restart - don't loop by default
+
 let mlmm;
 let mlannmean;
 let mlgrow;
 let glmm;
 let glannmean;
-let dec, avg, trend, days;
-let r, g, b, a;
+let date, avg, trend;
 let data;
 const padding = 10; // grid border padding
-const cellSize = 10; // grid cell size
+const cellSize = 20; // grid cell size
 const canvasWidth = 1000;
 const canvasHeight = 750;
-const canvasColor = '#2e382e';
-const rows = (canvasHeight / cellSize) - 2;
+const rows = (canvasHeight / cellSize);
 const columns = (canvasWidth - (padding * 2)) / cellSize;
 const cellCount = rows * columns;
 const colors = [
@@ -50,56 +60,133 @@ const colors = [
 ];
 let cells = new Array(cellCount).fill(0); // fill color array
 cells[0] = 1; // colors the 0 position cell the 1 value from the color array
-function preload() {
-  mlmm = loadJSON('../assets/climate-data/co2/co2-mm-mlo.json');
-  mlannmean = loadJSON('../assets/climate-data/co2/co2-annmean-mlo.json');
+function cellIndex(x, y) {
+  return (y * columns) + x;
 }
-function renderCells() {
+function preload() {
+  mlmm = loadJSON('../assets/climate-data/co2/co2-mm-mlo.json', 'json');
+  mlannmean = loadJSON('../assets/climate-data/co2/co2-annmean-mlo.json', 'json');
+}
+function setup() {
+  createCanvas(canvasWidth, canvasHeight); // canvas at 1000 * 750
+  background(colors[0]);
+  mlmm = Object.values(mlmm); // turn mlmm JSON into values
+}
+
+function draw() {
+  frameRate(2);
   let left = padding;
   let top = padding;
   let end = left + columns * cellSize;
-  for (let i = 0; i < cells.length; i++) {
-    noStroke();
-    fill(colors[cells[i]]);
-    let size = cellSize - 2;
-    rect(left, top, size, size, 0);
+  let size = cellSize - 2;
+  // push();
+  // for (let i = 0; i <= cells.length; i++) { // render bg cells
+  //   noStroke();
+  //   fill(colors[8]);
+  //   rect(left, top, size, size);
+  //   left += cellSize;
+  //   if (left === end) {
+  //     left = padding;
+  //     top += cellSize;
+  //   }
+  // }
+  // pop();
+  for (let j = 0; j <= mlmm.length; j++) { // render data cells
+    date = mlmm[j].date; // decimal date value into a variable
+    avg = mlmm[j].average; // average value into a variable
+    trend = mlmm[j].trend; // trend value into a variable
+    if (avg > 0 && avg < 320) {
+      fill(colors[2]);
+    }
+    if (avg >= 320 && avg < 325) {
+      fill(colors[3]);
+    }
+    if (avg >= 325 && avg < 330) {
+      fill(colors[4]);
+    }
+    if (avg >= 330 && avg < 335) {
+      fill(colors[5]);
+    }
+    if (avg >= 335 && avg < 340) {
+      fill(colors[6]);
+    }
+    if (avg >= 340 && avg < 345) {
+      fill(colors[7]);
+    }
+    if (avg >= 345 && avg < 350) {
+      fill(colors[8]);
+    }
+    if (avg >= 350 && avg < 355) {
+      fill(colors[9]);
+    }
+    if (avg >= 360 && avg < 365) {
+      fill(colors[10]);
+    }
+    if (avg >= 365 && avg < 370) {
+      fill(colors[11]);
+    }
+    if (avg >= 370 && avg < 375) {
+      fill(colors[12]);
+    }
+    if (avg >= 375 && avg < 380) {
+      fill(colors[13]);
+    }
+    if (avg >= 380 && avg < 385) {
+      fill(colors[14]);
+    }
+    if (avg >= 385 && avg < 390) {
+      fill(colors[15]);
+    }
+    if (avg >= 390 && avg < 395) {
+      fill(colors[16]);
+    }
+    if (avg >= 395 && avg < 400) {
+      fill(colors[17]);
+    }
+    if (avg >= 400 && avg < 405) {
+      fill(colors[18]);
+    }
+    if (avg >= 405 && avg < 410) {
+      fill(colors[19]);
+    }
+    if (avg >= 410 && avg < 415) {
+      fill(colors[20]);
+    }
+    if (avg >= 415 && avg < 420) {
+      fill(colors[21]);
+    }
+    if (avg >= 420 && avg < 425) {
+      fill(colors[22]);
+    }
+    // if (trend === -1) {
+    //   stroke(colors[15]);
+    //   strokeWeight(1);
+    // }
+    // if (trend >= 0 && trend <= 15) {
+    //   stroke(colors[18]);
+    //   strokeWeight(1);
+    // }
+    // if (trend > 15 && trend <= 20) {
+    //   stroke(colors[21]);
+    //   strokeWeight(1);
+    // }
+    // if (trend > 20 && trend <= 25) {
+    //   stroke(colors[24]);
+    //   strokeWeight(1);
+    // }
+    // if (trend > 25 && trend <= 30) {
+    //   stroke(colors[27]);
+    //   strokeWeight(1);
+    // }
+    // if (trend > 30) {
+    //   stroke(colors[30]);
+    //   strokeWeight(1);
+    // }
+    rect(left, top, size, size);
     left += cellSize;
     if (left === end) {
       left = padding;
       top += cellSize;
     }
-  }
-}
-function cellIndex(x, y) {
-  return (y * columns) + x;
-}
-console.log(cellIndex(97, 0));
-function setup() {
-  console.log(cellCount, colors, cells); // check cell count
-  createCanvas(canvasWidth, canvasHeight); // canvas at 1000 * 750
-  background(canvasColor);
-  // for (let i = 0; i < 100; i++) { // colors a particular cell the array color at position 1
-  //   cells[cellIndex(i, i)] = 1;
-  // }
-  renderCells();
-  mlmm = Object.values(mlmm); // turn mlmm JSON into values
-}
-function draw() {
-  frameRate(3);
-  // loop through mlmm JSON data
-  for (let i = 0; i <= mlmm.length; i++) {
-      dec = mlmm[i].decimaldate; // decimal date value into a variable
-      avg = mlmm[i].average; // average value into a variable
-      trend = mlmm[i].trend; // trend value into a variable
-      days = mlmm[i].numdays; // number of days value into a variable
-      // data = map(cellIndex[i, i], cellIndex.x, cellIndex.y, ); // map each cell to a decimal date
-
-      translate(10, 0);
-      push();
-        console.log(dec, avg, trend, days);
-        stroke(random(0, 255));
-        strokeWeight(15);
-        point(random(100, 900), random(100, 600), i);
-
   }
 }
